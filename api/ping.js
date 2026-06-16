b60df205a50a2a8bab2d78df963a03861cd206ca
jpeguero <jpeguero@gmail.com>
2026-06-16 05:54:28 -0500

    style: trigger rebuild to apply new Vercel env vars

--- api/ping.js
+++ api/ping.js
@@ -1,4 +1,5 @@
 export default function handler(req, res) {
+  // Trigger build to apply new Vercel environment variables
   return res.status(200).json({
     success: true,
     message: "Pawffeur API is live",
